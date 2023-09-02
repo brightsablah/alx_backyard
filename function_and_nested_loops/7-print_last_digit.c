707ec22ff2fdd9b82d05df96a73f76ccd16ed53e
Bright <bright.sablah@outlook.com>
2023-09-02 16:47:54 +0000

    task 7 testing

---
+++ function_and_nested_loops/7-print_last_digit.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+* print_last_digit - prints last digit of an int
+* @a: input int value
+*
+* Return: Always 0 (Success)
+*/
+
+int print_last_digit(int a)
+{
+int r;
+int b;
+
+/* making number absolute */
+	if (a < 0)
+	{
+		r = -a;
+	}
+
+	else
+	{
+		r = a;
+	}
+
+/* returning the last digit */
+
+	b = r;
+
+	while (r > 9)
+	{
+		r = r % 10;
+		b = r;
+	}
+
+return (b);
+
+}
