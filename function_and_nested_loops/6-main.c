228c3ff0b21a661b8547e61f79866a37b123b401
Bright <bright.sablah@outlook.com>
2023-09-02 15:14:30 +0000

    task 6 solved

---
+++ function_and_nested_loops/6-main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check the code
+* Description:
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+
+
+return (0);
+
+}
