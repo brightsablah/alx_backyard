d4880f66f6a3ef68cc0b1e57d612dad7d8273742
Bright <bright.sablah@outlook.com>
2023-09-05 00:50:54 +0000

    task 4 completed

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_nost_numbers - prints numbers from 0 - 9 not 2 and 4
+* Description:
+*
+* Return: Always 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+	}
+
+	_putchar('\n');
+
+}
