6852a420c7b6f1e1a304b1f2a8ce9b094eef13c3
Bright <bright.sablah@outlook.com>
2023-09-13 00:06:34 +0000

    task 1 complete!

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main -
+* Description:
+*
+* Return: Always 0 (Success)
+*/
+
+void swap_int(int* a, int* b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	
+
+}
