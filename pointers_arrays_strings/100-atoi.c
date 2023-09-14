bbc3ce2f8c31ccddb559743b335e2698253ff329
Bright <bright.sablah@outlook.com>
2023-09-14 14:32:45 +0000

    task 10 completed

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,19 +13,19 @@ int _atoi(char *s)
 	int count, sign, intg;
 	unsigned int stoi;
 
-	count = 0;
-	stoi = 0;
-	sign = 1;
-	intg = 0;
+	count = 0; /* counter to loop through the string array' */
+	stoi = 0; /* variable to store converted strings */
+	sign = 1; /* variable to account for the sign of the converted int */
+	intg = 0; /* changes to 1 if an int is encounted */
 
-	while (s[count])
+	while (s[count] != '\0')
 	{
-		if (s[count] == 45)
+		if (s[count] == '-')
 		{
 			sign *= -1;
 		}
 
-		while (s[count] >= 0 && s[count] <= 9)
+		while (s[count] >= '0' && s[count] <= '9')
 		{
 			intg = 1;
 			stoi = (stoi * 10) + (s[count] - '0');
--- pointers_arrays_strings/100-main.c
+++ pointers_arrays_strings/100-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 * main - check the code
 *
 * Return: Always 0.
 */
+
 int main(void)
 {
 int nb;
+
 	nb = _atoi("98");
 	printf("%d\n", nb);
 	nb = _atoi("-402");
@@ -20,8 +23,7 @@ int nb;
 	printf("%d\n", nb);
 	nb = _atoi("Suite 402");
 	printf("%d\n", nb);
-	nb = _atoi(" + + - -98 Battery Street; San Francisc
-o, CA 94111 - USA ");
+	nb = _atoi(" + + - -98 Battery Street; San Francisco, CA 94111 - USA ");
 	printf("%d\n", nb);
 	nb = _atoi("---++++ -++ Sui - te - 402 #cisfun :)");
 	printf("%d\n", nb);
