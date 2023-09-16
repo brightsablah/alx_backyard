25a0763dfd36017865fe3abd18336075a18f448f
Bright <bright.sablah@outlook.com>
2023-09-16 23:18:46 +0000

    still working on task 4

---
+++ more_pointers_arrays_and_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Description:
+*
+* Return: Always 0 (Success)
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	i = n;
+	while(i < n)
+	{
+		if (i != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+return (0);
+
+}
