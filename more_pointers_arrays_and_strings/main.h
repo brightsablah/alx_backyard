067b11ea5922a4ad3ce7448af98c1c54b32de97a
Bright <bright.sablah@outlook.com>
2023-09-16 04:56:08 +0000

    added all function prototypes for this project tasks into the main header file

--- more_pointers_arrays_and_strings/main.h
+++ more_pointers_arrays_and_strings/main.h
@@ -1,19 +1,16 @@
 #ifndef MAIN_FILE
 #define MAIN_FILE
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
+char *_stract(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+void print_buffer(char *b, int size);
 
 #endif
