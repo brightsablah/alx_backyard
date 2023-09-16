42936f9810d25937dcdd5a815cb6fd1cfea37ba9
Bright <bright.sablah@outlook.com>
2023-09-16 22:11:18 +0000

    task 2 initial commit

---
+++ more_pointers_arrays_and_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+* _strncpy - copies a string
+* Description:
+* @dest: pointer to concatenated string
+* @src: pointer to string to be added to dest
+* @n: number of characters of src to be added to dest
+* Return: pointer to dest variable
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+
+}
