2a829de07e7ed6d054c122e47da3e5d8387ee0c3
Bright <bright.sablah@outlook.com>
2023-09-25 08:31:49 +0000

    task 8 completed

---
+++ more_pointers_arrays_and_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* rot13 - encodes string into rot13
+* Description: converts all specified characters into
+* specified codes
+* @str: pointer to string input
+* Return: Pointer to converted string
+*/
+
+
+char *rot13(char *str)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+				break;
+			}
+		}
+	}
+return (str);
+
+}
