6db6ac6a67913ea6679319047709cce82e6118d1
Bright <bright.sablah@outlook.com>
2023-09-27 03:32:39 +0000

    added bettyTemplate

--- recursion/main.h
+++ recursion/main.h
@@ -1,16 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-_putchar(char c);
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt_test(int, int);
 int is_prime_number(int n);
+int _prime_test(int, int);
 int is_palindrome(char *s);
+int palindrome_test(char *s, int, int);
 int wildcmp(char *s1, char *s2);
-
+int wilcard_check(char *s1, char *s2);
 
 #endif
