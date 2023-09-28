a8b66cfd3077476e2378cfac85e9c798552c071f
Bright <bright.sablah@outlook.com>
2023-09-28 03:32:15 +0000

    Added 1-strncat.c function

---
+++ static_libraries/1-strncat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+* _strncat - adds two strings
+* Description:
+* @dest: pointer to concatenated string
+* @src: pointer to string to be added to dest
+* @n: number of characters of src to be added to dest
+* Return: pointer to dest variable
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int dest_len = 0;
+	int src_len = 0;
+	int i;
+	char *pointer = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+		dest_len++;
+	}
+
+	while (*src != '\0')
+	{
+		src++;
+		src_len++;
+	}
+
+
+	dest -= dest_len;
+	src -= src_len;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	dest[dest_len + src_len] = '\0';
+
+
+return (pointer);
+
+}
