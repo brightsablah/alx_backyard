9cef07df5e37713133dae577a9b33d75a26479bc
Bright <bright.sablah@outlook.com>
2023-09-28 00:48:55 +0000

    question 0

---
+++ recursion_quiz/q#0.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print(int nb)
+{
+    if (nb < 0) 
+    {
+        return;
+    }
+    printf("%d", nb);
+    nb --;
+    print(nb);
+}
+
+int main(void)
+{
+    print(4);
+    return (0);
+}
