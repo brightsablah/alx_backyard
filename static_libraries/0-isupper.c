7cdd80a77f13e9eb29f3047e9ec6e3436197ff10
Bright <bright.sablah@outlook.com>
2023-09-28 03:32:15 +0000

    Added 0-isupper.c function

---
+++ static_libraries/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isupper - checks case of character input
+* @c: input character
+*
+* Return: Always 0 (Success)
+*/
+
+int _isupper(int c)
+{
+	int r = 0;
+
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		r = 1;
+	}
+
+return (r);
+
+}
