13c8ce2243c2ee091ffbe2bb0a68b12e77f3d2fc
Bright <bright.sablah@outlook.com>
2023-09-28 07:50:24 +0000

    function prints its name

---
+++ argc_and_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Description: prints name of program
+* @argc: argument count
+* @argv: argument vector
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+return (0);
+
+}
