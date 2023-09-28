9fe024e8194561745bfdbfbffba98b766355ae72
Bright <bright.sablah@outlook.com>
2023-09-28 05:15:12 +0000

    added custom putchar and created betty Template for this project

---
+++ argc_and_argv/bettyTemplate.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Description:
+* @argc: argument count
+* @argv: argument vector
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+
+
+return (0);
+
+}
