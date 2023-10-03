29cdc58c598aac39a9bcb7d20b30c8d63a5ec79e
Bright <bright.sablah@outlook.com>
2023-10-03 03:09:27 +0000

    added main header

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -8,7 +8,12 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+int _strlen(char *s);
 char **strtow(char *str);
+int word_count(char *str);
+char *_strncpy(char *dest, char *src, int n);
+int all_spaces(char *str);
+char *allocate_word(char *str, int length);
 
 
 #endif
