03e3a643d364fa79c1b07a4c8e58737a8e4aa62d
Bright <bright.sablah@outlook.com>
2023-10-08 02:06:13 +0000

    task 0 completed

---
+++ structures-typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - sprecifications of dog
+* @name: name of dog
+* @age: age of dog
+* @owner: name of owner of dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
