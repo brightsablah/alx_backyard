d5aa451094cca87c5845a1b0cb4d3e12b7e44286
Bright <bright.sablah@outlook.com>
2023-12-13 04:37:32 +0000

    4 complete

---
+++ c-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* free_listint - frees a singly lisnked list
+* @head: head of list
+* Return: void
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *current_node;
+
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node);
+	}
+}
