01bf62e6b1420f0d7679ff6ae43d4da2b949f4e6
Bright <bright.sablah@outlook.com>
2023-12-29 19:29:53 +0000

    0 done

--- c-bit_manipulation/0-binary_to_uint.c
+++ c-bit_manipulation/0-binary_to_uint.c
@@ -11,22 +11,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
-	int len = 0, i;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[len] != '\0')
+	while (b[i] != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-		len++;
-	}
+		if (b[i] == '0' || b[i] == '1')
+		{
+			dec = (dec << 1) | (b[i] - '0');
+			i++;
+		}
+		else
+		{
+			return (0); /* non digit character found */
+		}
 
-	for (i = 0; i < len; i++)
-	{
-		if (b[i] == '1')
-			dec += 1 << (len - i - 1);
 	}
 
 	return (dec);
