cb94311521005b094d7aa1f350a86cfbd5a86adc
Bright <bright.sablah@outlook.com>
2023-12-30 09:50:43 +0000

    clear git done

---
+++ c-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets the value of a bit at a given index to 0
+ * @n: The unsigned long int to extract the bit from
+ * @index: The index of the bit to retrieve (starting from 0)
+ *
+ * Return:
+ * The value of the bit at the specified index (0 or 1),
+ * or -1 if an error occurred (if index is out of range).
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int max_index = sizeof(*n) * 8 - 1;
+	unsigned long int mask = ~(1UL << index);
+
+	if (index > max_index)
+		return (-1);
+
+	*n &= mask;
+
+	return (1);
+}
